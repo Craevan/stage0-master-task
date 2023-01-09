acfd287eb97211db10d93ea6b18fa1f8a4b585c5
Crevan <a.siverskii@gmail.com>
2023-01-09 19:50:33 +0100

    [add] Task 1. Numbers reverter solution

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,14 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int lastNumber = number % 10;
+        int middleNumber = (number / 10) % 10;
+        int firstNumber = number / 100;
+        printNumber(lastNumber, middleNumber, firstNumber);
+    }
+
+    private void printNumber(int x, int y, int z) {
+        System.out.printf("%d%d%d\n", x, y, z);
     }
 
 }
