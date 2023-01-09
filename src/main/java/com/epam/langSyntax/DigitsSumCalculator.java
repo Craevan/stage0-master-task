5e3523be759e0cd7f940145857d47d8a9cf96012
Crevan <a.siverskii@gmail.com>
2023-01-09 19:50:11 +0100

    [add] Task 2. Digits sum calculator solution

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,15 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int lastDigit = number % 10;
+        int middleDigit1 = (number / 10) % 10;
+        int middleDigit2 = (number / 100) % 10;
+        int firstDigit = number / 1000;
+        printSum(firstDigit, middleDigit1, middleDigit2, lastDigit);
+    }
+
+    private void printSum(int x, int y, int z, int w) {
+        System.out.println(x + y + z + w);
     }
 
 }
