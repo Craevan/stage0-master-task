2fc048c6403af999cf1f06fb6b5de81bcfe06ce0
Crevan <a.siverskii@gmail.com>
2023-01-10 17:59:34 +0100

    [add] solutions for task "loops"

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,11 +3,28 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            spacePrint(cathetusLength - i - 1);
+            printNumberInReverseOrder(i);
+            System.out.print("*");
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
+    }
+
+    private void spacePrint(int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    private void printNumberInReverseOrder(int number) {
+        for (int i = number; i > 1; i--) {
+            System.out.print("*");
+        }
     }
 
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,11 +3,35 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            spacePrint(cathetusLength - i - 1);
+            printNumberInReverseOrder(i);
+            System.out.print(1);
+            printNumberInNaturalOrder(i);
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
+    }
+
+    private void spacePrint(int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    private void printNumberInNaturalOrder(int number) {
+        for (int i = 1; i < number; i++) {
+            System.out.print(i + 1);
+        }
+    }
+
+    private void printNumberInReverseOrder(int number) {
+        for (int i = number; i > 1; i--) {
+            System.out.print(i);
+        }
     }
 
 }
